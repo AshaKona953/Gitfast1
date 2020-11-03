a192035d933639db384e18cab2669ff0d89f1b77
AshaKona <konaashar131953@gmail.com>
2020-11-03 23:12:36 +0530

    jf

--- src/newproj/test1.java
+++ src/newproj/test1.java
@@ -11,7 +11,7 @@ public class test1 {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Asha kona\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com");
-	    System.out.println("i am Asha");
+	    System.out.println("Adithya");
 	}
 
 }
